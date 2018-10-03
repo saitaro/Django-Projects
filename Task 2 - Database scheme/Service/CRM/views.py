36651e1de8bf477966816bda7c8b768274f5d428
Ripsodi <ripsodi@gmail.com>
2018-10-04 02:05:32 +0300

    Delete Group

--- Task 2 - Database scheme/Service/CRM/views.py
+++ Task 2 - Database scheme/Service/CRM/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from .models import *
 from rest_framework import viewsets, filters
 from django_filters import rest_framework
